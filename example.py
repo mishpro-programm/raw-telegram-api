6d607f7af3256e5664557c0aafc529d971090c8a
mishpro <root@venusbiser.ru>
2023-06-09 19:41:14 +0700

    Add files via upload

--- example.py
+++ example.py
@@ -1,9 +1,4 @@
-# raw-telegram-api
-Raw Telegram API for Python 3\
-#Example
-```python
 from raw-telegram.api import APIClient
-token = "<paste your token here>"
+token = "<your token>"
 api = APIClient(token)
 api.send_method("sendMessage", {'chat_id': 5102762920, 'text': 'Hello world!'})
-```
