f6354a8eb5d1af462a7588828f02ec10a0c55f38
mishpro <root@venusbiser.ru>
2023-06-09 20:12:07 +0700

    dependencies

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from distutils.core import setup
-setup(version = "1.0",
+setup(version = "1.1",
       name = "raw-telegram-api",
       description = "Raw API for telegram bots",
       author = "mishpro",
       author_email = "root@venusbiser.ru",
-      packages = ["raw-telegram"])
+      packages = ["raw_telegram"],
+      install_requires = ["requests>=2.30.0"])
